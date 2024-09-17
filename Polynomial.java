1c5f188237b74e3f75f44d8ebd603e169166c696
bryntam <112902843+bryntam@users.noreply.github.com>
2024-09-17 11:48:17 -0400

    added Polynomial.java and Driver.java

---
+++ Polynomial.java
@@ -0,0 +1,11 @@
+class Polynomial {
+    double coefficients[];
+
+    public Polynomial() {
+        coefficients = new double[0];
+    }
+    public Polynomial(double[] coefficients) {
+        this.coefficients = coefficients;
+    }
+    
+}
