33f4a7c5b669cc3da753de020d68c828834ef068
bryntam <112902843+bryntam@users.noreply.github.com>
2024-09-22 22:52:56 -0400

    added Polynomial and Driver

--- Polynomial.java
+++ Polynomial.java
@@ -1,11 +1,36 @@
-class Polynomial {
-    double coefficients[];
+public class Polynomial {
+    private double[] coefficients;
 
     public Polynomial() {
-        coefficients = new double[0];
+        this.coefficients = new double[] { 0 };
     }
+
     public Polynomial(double[] coefficients) {
         this.coefficients = coefficients;
     }
-    
+
+    public Polynomial add(Polynomial other) {
+        int maxLength = Math.max(this.coefficients.length, other.coefficients.length);
+        double[] resultCoefficients = new double[maxLength];
+
+        for (int i = 0; i < maxLength; i++) {
+            double thisCoeff = i < this.coefficients.length ? this.coefficients[i] : 0;
+            double otherCoeff = i < other.coefficients.length ? other.coefficients[i] : 0;
+            resultCoefficients[i] = thisCoeff + otherCoeff;
+        }
+
+        return new Polynomial(resultCoefficients);
+    }
+
+    public double evaluate(double x) {
+        double result = 0;
+        for (int i = 0; i < coefficients.length; i++) {
+            result += coefficients[i] * Math.pow(x, i);
+        }
+        return result;
+    }
+
+    public boolean hasRoot(double x) {
+        return evaluate(x) == 0;
+    }
 }
